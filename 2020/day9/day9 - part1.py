4595994777a117a12b8314df86b5d1ad7cd9dd4e
nalo26 <nath.v26@gmail.com>
2020-12-09 09:33:48 +0100

    day9

---
+++ 2020/day9/day9 - part1.py
@@ -0,0 +1,17 @@
+file = open("input.txt")
+lines = [int(line.replace("\n", "")) for line in file.readlines()]
+
+def isSumOfTwoPreamble(n, preamble):
+    for a in preamble:
+        for b in preamble:
+            if a == b: continue
+            if a + b == n: return True
+    return False
+
+
+i = 25
+for inp in lines[25:]:
+    if not isSumOfTwoPreamble(inp, lines[i-25:][:25]):
+        print(inp)
+        break
+    i += 1
---
+++ 2020/day9/day9 - part2.py
@@ -0,0 +1,30 @@
+import sys
+
+file = open("input.txt")
+lines = [int(line.replace("\n", "")) for line in file.readlines()]
+
+def isSumOfTwoPreamble(n, preamble):
+    for a in preamble:
+        for b in preamble:
+            if a == b: continue
+            if a + b == n: return True
+    return False
+
+
+invalidNumber = 0
+i = 25
+for inp in lines[25:]:
+    if not isSumOfTwoPreamble(inp, lines[i-25:][:25]):
+        # print(inp)
+        invalidNumber = inp
+        break
+    i += 1
+    
+
+for i in range(2, len(lines)):
+    for n in range(len(lines)-i):
+        toCheck = lines[n:][:i]
+        if sum(toCheck) == invalidNumber:
+            print(i, n, toCheck)
+            print(min(toCheck) + max(toCheck))
+            sys.exit()
