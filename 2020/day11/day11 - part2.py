72bfaf941ff7d99a1ebc32864f48624aa98139e3
nalo26 <nath.v26@gmail.com>
2020-12-11 12:10:11 +0100

    day 11

---
+++ 2020/day11/day11 - part2.py
@@ -0,0 +1,57 @@
+file = open("input.txt")
+seats = [list(line.replace("\n", "")) for line in file.readlines()]
+last = [row[:] for row in seats]
+
+floor    = "."
+empty    = "L"
+occupied = "#"
+
+def isOccupied(seats, x, y, direction):
+    if x < 0 or y < 0: return False
+    try:
+        while seats[y][x] == floor:
+            x += direction[0]
+            y += direction[1]
+            if x < 0 or y < 0: return False
+        return seats[y][x] == occupied
+    except IndexError: return False
+    
+def isEmpty(seats, x, y, direction):
+    if x < 0 or y < 0: return True
+    try:
+        while seats[y][x] == floor:
+            x += direction[0]
+            y += direction[1]
+            if x < 0 or y < 0: return True
+        return seats[y][x] != occupied
+    except IndexError: return True
+
+def shouldBeOccupied(seats, x, y):
+    toTry = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+    return len([test for test in toTry if not isOccupied(seats, x+test[0], y+test[1], test)]) == len(toTry)
+
+def shouldBeEmpty(seats, x, y):
+    toTry = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+    return len([test for test in toTry if not isEmpty(seats, x+test[0], y+test[1], test)]) >= 5
+
+def showMap(seats):
+    for line in seats:
+        print("".join(line))
+    print()
+
+
+while True:
+    for y in range(len(seats)):
+        for x in range(len(seats[y])):
+            if last[y][x] == floor   : continue
+            if last[y][x] == empty   : seats[y][x] = occupied if shouldBeOccupied(last, x, y) else empty
+            if last[y][x] == occupied: seats[y][x] = empty if shouldBeEmpty(last, x, y) else occupied
+    if last == seats: break
+    last = [row[:] for row in seats]
+            
+
+res = 0
+for seat in seats:
+    res += len([s for s in seat if s == occupied])
+    
+print(res)
