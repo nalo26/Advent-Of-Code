b0a1b98bdb6e1ce9da5d40f1aa91e2971580dcf9
Émilie Vey <emilie.vey@etu.univ-grenoble-alpes.fr>
2021-12-04 08:55:00 +0100

    [2021] day4 - pretty strong but fun one!

---
+++ 2021/day4/day4 - part2.py
@@ -0,0 +1,55 @@
+class Board:
+    def __init__(self):
+        self.grid = []
+        self.rows = []
+        self.cols = []
+        self.coords = []
+        self.win = False
+        
+    def draw(self, value):
+        for i, row in enumerate(self.grid):
+            if value in row:
+                r, c = i, row.index(value)
+                self.rows.append(r)
+                self.cols.append(c)
+                self.coords.append((r, c))
+                return self.checkVictory(value, r, c)
+            
+    def checkVictory(self, value, row_val, col_val):
+        if self.rows.count(row_val) == 5 or self.cols.count(col_val) == 5:
+            _sum = 0
+            for y, row in enumerate(self.grid):
+                for x, col in enumerate(row):
+                    if (y, x) not in self.coords: _sum += col
+            self.win = True
+            return _sum * value
+        
+        return None
+        
+    
+file = open("input.txt")
+lines = file.readlines()
+
+draw = list(map(int, lines[0].split(",")))
+boards = []
+
+# parsing each board
+for board_i in range(2, len(lines), 6): # step by step each board
+    board = Board()
+    for grid_line in lines[board_i:][:5]:
+        # parsing each str row to int list
+        board.grid.append(
+            [int(grid_line[i:i+3]) for i in range(0, len(grid_line), 3)]
+        )
+    boards.append(board)
+        
+# making the draws
+for d in draw:
+    board_to_remove = []
+    for b, board in enumerate(boards):
+        victory = board.draw(d)
+        if victory is not None:
+            print(victory)
+            board_to_remove.append(b)
+            
+    boards = [board for board in boards if not board.win]
