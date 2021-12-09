2c63cdd4caf85ebf06bd94a8cc645842bb675fee
nalo26 <nath.v26@gmail.com>
2021-12-09 08:03:23 +0100

    [2021] day 8 - part 1 only :c

---
+++ 2021/day8/day8 - part2.py
@@ -0,0 +1,12 @@
+file = open("input.txt")
+
+res = 0
+for line in file.readlines():
+    inp, out = line.replace("\n", "").split(" | ")
+    inp = inp.split(" ")
+    out = out.split(" ")
+    for o in out:
+        if len(o) in (2, 4, 3, 7): 
+            res += 1
+
+print(res)
