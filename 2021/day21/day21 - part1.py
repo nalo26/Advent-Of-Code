23f97e39f5eb3584b958c9c6a8595767295a19ba
nalo26 <nath.v26@gmail.com>
2021-12-21 10:27:10 +0100

    [2021] day21 - my brain hurts

---
+++ 2021/day21/day21 - part1.py
@@ -0,0 +1,27 @@
+file = open("input.txt")
+
+class Player:
+    def __init__(self, position):
+        self.position = position
+        self.score = 0
+
+
+p1 = Player(int(file.readline().split(": ")[1]))
+p2 = Player(int(file.readline().split(": ")[1]))
+
+players = [p1, p2]
+p_index = 0
+dice = 0
+while p1.score < 1000 and p2.score < 1000:
+    index = p_index % 2
+    score = 0
+    for _ in range(3):
+        dice += 1
+        score += (dice - 1) % 100 + 1
+    players[index].position += score
+    players[index].position = (players[index].position - 1) % 10 + 1
+    players[index].score += players[index].position
+    p_index += 1
+
+if p1.score >= 1000: print(p2.score * dice)
+else: print(p1.score * dice)
---
+++ 2021/day21/day21 - part2.py
@@ -0,0 +1,33 @@
+import itertools
+from functools import cache
+
+file = open("input.txt")
+
+p1 = int(file.readline().split(": ")[1])
+p2 = int(file.readline().split(": ")[1])
+
+WIN_SCORE = 21
+
+@cache
+def play(p1, p2, p1_score = 0, p2_score = 0):
+    win_amount = [0, 0]
+    for roll1 in itertools.product([1,2,3], repeat=3):
+        for roll2 in itertools.product([1,2,3], repeat=3):
+            _p1 = (sum(roll1) + p1 - 1) % 10 + 1
+            _p2 = (sum(roll2) + p2 - 1) % 10 + 1
+            _p1_score = p1_score + _p1
+            _p2_score = p2_score + _p2
+
+            if _p1_score >= WIN_SCORE:
+                win_amount[0] += 1
+                break
+            if _p2_score >= WIN_SCORE:
+                win_amount[1] += 1
+                continue
+
+            step = play(_p1, _p2, _p1_score, _p2_score)
+            win_amount[0] += step[0]
+            win_amount[1] += step[1]
+    return win_amount
+
+print(max(play(p1, p2)))
