5c245d601b2bec98c4cee9408fb4c37283c54e8a
nalo26 <nath.v26@gmail.com>
2022-01-06 10:44:19 +0100

    [2015] day4

---
+++ 2015/day4/day4 - part1.py
@@ -0,0 +1,12 @@
+from hashlib import md5
+
+key = open("input.txt").readline()
+
+answer = 0
+while True:
+    to_code = key + str(answer)
+    hash = md5(to_code.encode()).hexdigest()
+    if hash.startswith("00000"): break
+    answer += 1
+
+print(answer)
