05a7245a8a669882d1fba414b98b6d45c0b94504
nalo26 <nath.v26@gmail.com>
2022-12-15 12:06:12 +0100

    [2022] day15 - part1 only, part2 is too greedy

--- 2022/README.md
+++ 2022/README.md
@@ -36,8 +36,8 @@ Everyting is made with **Python 3**. A lot of programs need at least Python 3.6,
 | `day13 - part2.py` | :heavy_check_mark: | :heavy_check_mark: |
 | `day14 - part1.py` | :heavy_check_mark: | :heavy_check_mark: |
 | `day14 - part2.py` | :heavy_check_mark: | :heavy_check_mark: |
-| `day15 - part1.py` | :hourglass: | :hourglass: |
-| `day15 - part2.py` | :hourglass: | :hourglass: |
+| `day15 - part1.py` | :heavy_check_mark: | :heavy_check_mark: |
+| `day15 - part2.py` | :x: | :x: |
 | `day16 - part1.py` | :hourglass: | :hourglass: |
 | `day16 - part2.py` | :hourglass: | :hourglass: |
 | `day17 - part1.py` | :hourglass: | :hourglass: |
---
+++ 2022/day15/day15 - part1.py
@@ -0,0 +1,32 @@
+import re
+from tqdm import tqdm
+
+
+file = open("input.txt")
+lines = file.read().splitlines()
+
+def dist(a: tuple[int, int], b: tuple[int, int]) -> int:
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+sensors = {}
+distances = set()
+xs = set()
+for line in lines:
+    match = re.search("Sensor at x=(.*), y=(.*): closest beacon is at x=(.*), y=(.*)", line)
+    sx, sy, bx, by = tuple(map(int, match.groups()))
+    sensors[(sx, sy)] = (bx, by)
+    distances.add(dist((sx, sy), (bx, by)))
+    xs |= {sx, bx}
+
+row = 2000000
+count = 0
+for x in tqdm(range(min(xs) - max(distances), max(xs) + max(distances))):
+    if (x, row) in set(sensors.values()):
+        continue
+    for sensor, beacon in sensors.items():
+        if dist(sensor, (x, row)) <= dist(sensor, beacon):
+            count += 1
+            break
+    
+print(count)
