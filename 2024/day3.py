ce5fbd7236a53d9a8541a8eb5c395581bfede3c3
nalo26 <me@nalo.dev>
2024-12-03 08:17:57 +0100

    [2024] day3 - It's regex time :floppy_disk:

---
+++ 2024/day3.py
@@ -0,0 +1,29 @@
+import re
+
+file = open("input.txt")
+lines = file.read()
+
+regex = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+
+
+def part1():
+    res = 0
+    for find in re.findall(regex, lines):
+        res += int(find[0]) * int(find[1])
+    return res
+
+
+def part2():
+    res = 0
+    # adding a do() at the start to make the first split work
+    for does in ("do()" + lines).split("don't()"):
+        # ignore disable instruction parts, keeping only the do() parts
+        instr = "".join(does.split("do()")[1:])
+        for find in re.findall(regex, instr):
+            res += int(find[0]) * int(find[1])
+    return res
+
+
+print("Part 1:", part1())
+print("Part 2:", part2())
+file.close()
