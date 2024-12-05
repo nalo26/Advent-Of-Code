31e2844de32f58f2cf11cda3aff1e44a7144c8e2
nalo26 <me@nalo.dev>
2024-12-05 08:56:48 +0100

    [2024] day5 - graphs save lifes :printer:

---
+++ 2024/day5.py
@@ -0,0 +1,40 @@
+import networkx as nx
+
+file = open("input.txt")
+lines = file.read().split("\n\n")
+
+rules = [tuple(map(int, line.split("|"))) for line in lines[0].splitlines()]
+updates = [list(map(int, line.split(","))) for line in lines[1].splitlines()]
+
+invalid_updates = updates.copy()
+
+
+def part1():
+    res = 0
+    for update in updates:
+        for rule in rules:
+            if rule[0] in update and rule[1] in update and update.index(rule[0]) > update.index(rule[1]):
+                break
+        else:
+            invalid_updates.remove(update)  # for part 2
+            res += update[len(update) // 2]
+    return res
+
+
+def part2():
+    res = 0
+    for update in invalid_updates:
+        matchin_rules = []
+        for rule in rules:
+            if rule[0] in update and rule[1] in update:
+                matchin_rules.append(rule)
+        graph = nx.DiGraph()
+        graph.add_edges_from(matchin_rules)
+        sorted_updates = list(nx.topological_sort(graph))
+        res += sorted_updates[len(sorted_updates) // 2]
+    return res
+
+
+print("Part 1:", part1())
+print("Part 2:", part2())
+file.close()
